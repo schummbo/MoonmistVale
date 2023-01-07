1209439f148c60fc41cc968e084155bc63e5076b
Schummbo <kschumm@gmail.com>
2023-01-06 19:56:38 -0800

    Walking and idle animations. Directional movement.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(PlayerInput))]
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2d;
+    private Animator animator;
+
+    [SerializeField] public float Speed = 2f;
+
+    private Vector2 motionVector;
+    private Vector2 lastDirection;
+
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        if (motionVector != Vector2.zero)
+        {
+            animator.Play("MovementTree");
+            animator.SetFloat("Horizontal", motionVector.x);
+            animator.SetFloat("Vertical", motionVector.y);
+        }
+        else
+        {
+            animator.Play("IdleTree");
+            animator.SetFloat("Horizontal", lastDirection.x);
+            animator.SetFloat("Vertical", lastDirection.y);
+        }
+    }
+
+    void OnMove(InputValue input)
+    {
+        if (motionVector != Vector2.zero)
+        {
+            lastDirection = motionVector;
+        }
+
+        motionVector = input.Get<Vector2>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rigidbody2d.velocity = motionVector * Speed;
+    }
+}
