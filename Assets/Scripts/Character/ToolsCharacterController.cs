634747ed0ffdc1e835d7375630bc2fa7b20ac701
Schummbo <kschumm@gmail.com>
2023-01-13 00:11:17 -0800

    Switch use tool action to unity input system

--- Assets/Scripts/Character/ToolsCharacterController.cs
+++ Assets/Scripts/Character/ToolsCharacterController.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ToolsCharacterController : MonoBehaviour
 {
     private CharacterController characterController;
     private Rigidbody2D rigidBody2D;
+    private PlayerInput playerInput;
+
+    private InputAction useToolAction;
 
     [SerializeField] private float offsetDistance = 1f;
     [SerializeField] private float sizeOfInteractableArea = 1.2f;
@@ -12,16 +16,21 @@ public class ToolsCharacterController : MonoBehaviour
     {
         characterController = GetComponent<CharacterController>();
         rigidBody2D = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+
+        useToolAction = playerInput.actions["UseTool"];
     }
 
-    void Update()
+    void OnEnable()
     {
-        if (Input.GetMouseButton(0))
-        {
-            UseTool();
-        }
+        useToolAction.performed += UseTool;
     }
 
+    void OnDisable()
+    {
+        useToolAction.performed -= UseTool;
+    }
+    
     /// <summary>
     /// Checks the direction the character is facing.
     /// If any colliders in the area in front of the character has
@@ -31,7 +40,7 @@ public class ToolsCharacterController : MonoBehaviour
     ///
     /// For example, a tree will split into x number of logs.
     /// </summary>
-    private void UseTool()
+    private void UseTool(InputAction.CallbackContext context)
     {
         var position = rigidBody2D.position + characterController.LastDirection * offsetDistance;
 
