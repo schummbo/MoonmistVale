abd44d14541e5328829e2dde7ad7088008edd6c6
Schummbo <kschumm@gmail.com>
2023-01-28 23:41:44 -0800

    Dialog system.

--- Assets/Scripts/Interactables/TalkInteract.cs
+++ Assets/Scripts/Interactables/TalkInteract.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class TalkInteract : InteractableBase
 {
+    [SerializeField] private DialogContainer dialogContainer;
+
     public override bool IsInteractable => hasSomethingToSay;
 
     private bool hasSomethingToSay = true;
     
     public override void Interact(Character character)
     {
+        GameManager.Instance.DialogSystem.Initialize(dialogContainer);
         hasSomethingToSay = false;
-        Debug.Log("Talking with character.");
     }
 }
